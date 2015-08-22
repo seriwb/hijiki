073b32f1cc258226bc4e8ac761eae60b5a66cf02
seri <seri.wb@gmail.com>
2015-08-22 18:05:25 +0900

    adopt honoka style and create site sample

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,11 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("ひじき祭会場"))
+  }
+
+  def about = Action {
+    Ok(views.html.about("ひじき祭会場"))
   }
 
 }
--- public/javascripts/hijiki.js
+++ public/javascripts/hijiki.js
@@ -1,3 +1,7 @@
 if (window.console) {
   console.log("Welcome to ひじき祭！");
 }
+
+// bootstrap code
+$('.bs-component [data-toggle="popover"]').popover();
+$('.bs-component [data-toggle="tooltip"]').tooltip();
